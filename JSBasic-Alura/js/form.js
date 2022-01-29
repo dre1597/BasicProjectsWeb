3cd084fa678a3a9786d48f4dd35a41759f369723
André Pereira <andrepereira97@yahoo.com>
2022-01-29 13:57:57 -0300

    :recycle: set idendation to 2 and adjust some typos

--- JSBasic-Alura/js/form.js
+++ JSBasic-Alura/js/form.js
@@ -1,86 +1,77 @@
-const addBtn = document.querySelector("#add-patient")
+const addBtn = document.querySelector('#add-patient');
 
-addBtn.addEventListener("click" , (event) => {
-    event.preventDefault()
+addBtn.addEventListener('click', (event) => {
+  event.preventDefault();
 
-    const form = document.querySelector(".form")
+  const form = document.querySelector('.form');
 
-    let patient = patientFromForm(form)
+  let patient = patientFromForm(form);
 
-    let errors = validatePatient(patient)
+  let errors = validatePatient(patient);
 
-    if(errors.length > 0){
-        returnErrorMessagens(errors)
-        return;
-    }
+  if (errors.length > 0) {
+    returnErrorMessages(errors);
+    return;
+  }
 
-    addPatient(patient)
-    
+  addPatient(patient);
 
-    form.reset()
-    const ulErrors = document.querySelector('#error-menssage')
-    ulErrors.innerHTML = ""
-})
+  form.reset();
+  const ulErrors = document.querySelector('#error-message');
+  ulErrors.innerHTML = '';
+});
 
 function addPatient(patient) {
-
-    let patientTr = returnTr(patient)
-    const table = document.querySelector('#patients-table')
-    table.appendChild(patientTr)
-
+  let patientTr = returnTr(patient);
+  const table = document.querySelector('#patients-table');
+  table.appendChild(patientTr);
 }
 
-function returnErrorMessagens(errors){
-    const ul = document.querySelector('#error-menssage')
-    ul.innerHTML = ""
+function returnErrorMessages(errors) {
+  const ul = document.querySelector('#error-message');
+  ul.innerHTML = '';
 
-    errors.forEach( (erro) => {
-        let li = document.createElement("li")
-        li.textContent = erro
-        ul.appendChild(li)
-    })
+  errors.forEach((error) => {
+    let li = document.createElement('li');
+    li.textContent = error;
+    ul.appendChild(li);
+  });
 }
 
 function patientFromForm(form) {
-    let patient =  {
-        name: form.name.value,
-        weight: form.weight.value,
-        height: form.height.value,
-        fat: form.fat.value,
-        bmi: calcBmi(form.weight.value, form.height.value)
-    }
-
-    return patient
+  return {
+    name: form.name.value,
+    weight: form.weight.value,
+    height: form.height.value,
+    fat: form.fat.value,
+    bmi: calcBmi(form.weight.value, form.height.value),
+  };
 }
 
 function returnTr(patient) {
+  let patientTr = document.createElement('tr');
 
-    let patientTr = document.createElement("tr")
+  patientTr.appendChild(returnTd(patient.name, 'info-name'));
+  patientTr.appendChild(returnTd(patient.weight, 'info-weight'));
+  patientTr.appendChild(returnTd(patient.height, 'info-height'));
+  patientTr.appendChild(returnTd(patient.fat, 'info-fat'));
+  patientTr.appendChild(returnTd(patient.bmi, 'info-bmi'));
 
-    patientTr.appendChild(returnTd(patient.name, "info-name"))
-    patientTr.appendChild(returnTd(patient.weight, "info-weight"))
-    patientTr.appendChild(returnTd(patient.height, "info-height"))
-    patientTr.appendChild(returnTd(patient.fat, "info-fat"))
-    patientTr.appendChild(returnTd(patient.bmi, "info-bmi"))
-
-    return patientTr
-
+  return patientTr;
 }
 
 function returnTd(data, className) {
-    let td = document.createElement("td")
-    td.textContent = data
-    td.classList.add(className)
-    return td;
+  let td = document.createElement('td');
+  td.textContent = data;
+  td.classList.add(className);
+  return td;
 }
 
 function validatePatient(patient) {
+  let errors = [];
 
-    let errors = []
+  if (!validateWeight(patient.weight)) errors.push('Invalid weight');
+  if (!validateHeight(patient.height)) errors.push('Invalid height');
 
-    if(!validateWeight(patient.weight)) errors.push("Invalid weight")
-    if(!validateHeight(patient.height)) errors.push("Invalid height")
-
-    return errors
-
+  return errors;
 }
