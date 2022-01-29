f08224bd9d18783448d1721ea95ebfe7a4d39b5d
André Pereira <andrepereira97@yahoo.com>
2022-01-29 13:54:41 -0300

    :recycle: set identation to 2 and adjust some typos

--- HTMLBasic/assets/js/toggle.js
+++ HTMLBasic/assets/js/toggle.js
@@ -1,11 +1,11 @@
-const toggle = document.querySelector('.nav__toggle')
-const linksContainer = document.querySelector('.nav__links-container')
-const links = document.querySelector('.nav__links')
+const toggle = document.querySelector('.nav__toggle');
+const linksContainer = document.querySelector('.nav__links-container');
+const links = document.querySelector('.nav__links');
 
 toggle.addEventListener('click', () => {
-    const containerHeight = linksContainer.getBoundingClientRect().height
-    const linksHeight = links.getBoundingClientRect().height
-    
-    if (containerHeight === 0) linksContainer.style.height = `${linksHeight}px`
-    else linksContainer.style.height = 0
-})
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+
+  if (containerHeight === 0) linksContainer.style.height = `${linksHeight}px`;
+  else linksContainer.style.height = 0;
+});
